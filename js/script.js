a3a9554144a6d12d59f14b05422f59b17575fce3
Yaes2002 <deanhendriks2002@gmail.com>
2020-03-27 12:03:17 +0100

    launch

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const feedback = document.getElementById('feedback');
 const imageLocation = document.getElementById('imageLocation');
 const myDescription = document.getElementById('description');
 const myInventory = document.getElementById('inventory');
+const treasure = document.getElementById('treasure');
 
 let currentLocation = 4;
 
@@ -13,7 +14,7 @@ locations[0] = "kantine";
 locations[1] = "trap";
 locations[2] = "eind";
 locations[3] = "docentenkamer";
-locations[4] = "gang";
+locations[4] = "Gang";
 locations[5] = "medialab";
 locations[6] = "toiletten";
 locations[7] = "klaslokaal";
@@ -42,15 +43,19 @@ directions[7] = ["noord", "west", "oost"];
 directions[8] = ["noord", "west"];
 
 descriptions = [];
-descriptions[0] = "u staat in een kantine. Hier zitten studenten te eten of computerspelletjes te doen";
-descriptions[1] = "u staat op een trap naar de eerste etage. Om u heen lopen studenten omhoog en omlaag";
-descriptions[2] = "u heeft gewonnen";
-descriptions[3] = "u staat in de lerarenkamer. De leraren eten hier hun lunch of drinken koffie of thee";
-descriptions[4] = "u staat in een gang. Studenten en leraren lopen richting de klaslokalen";
-descriptions[5] = "u staat in het medialab. Hier kan geexperimenteerd worden met bijvoorbeeld virtual reality brillen";
-descriptions[6] = "u staat bij de toiletten";
-descriptions[7] = "u staat in een klaslokaal. De tafels staan recht achter elkaar en voorin is een projector en een smartboard";
-descriptions[8] = "u staat in het examenlokaal. Hier zijn de vierdejaars studenten bezig met het voorbereiden van hun examen";
+descriptions[0] = ["Je bevind jezelf in de kantine. Hier zitten studenten te eten of op hun laptop.", "", "",""];
+descriptions[1] = ["u staat op een trap naar de eerste etage. Er lopen veel studenten om je heen.", "", "",""];
+descriptions[2] = ["Je hebt gewonnen!", "", "","shag"];
+descriptions[3] = ["u staat in de lerarenkamer. De leraren eten hier hun lunch en chillen hier.", "Shag", "shag.png",""];
+descriptions[4] = ["u staat in de Gang. Studenten en leraren lopen rondom je heen", "", "",""];
+descriptions[5] = ["u staat in het medialab. ", "Steen", "rock.png",""];
+descriptions[6] = ["u staat bij de toiletten", "", "",""];
+descriptions[7] = ["u staat in een klaslokaal. ", "kaas", "kaas.png",""];
+descriptions[8] = ["u staat in het examenlokaal. Hier zijn de vierdejaars studenten bezig met het voorbereiden van hun examen", "", "",""];
+
+
+Inventory = [];
+
 
 myInput.addEventListener('keydown', getInput, false);
 
@@ -62,20 +67,20 @@ function getInput(evt) {
       if (directions[currentLocation].indexOf(inputArray[1]) != -1) {
         switch (inputArray[1]) {
           case "noord":
-            currentLocation -= 3;
+            if (Check(currentLocation))currentLocation -= 3;
             break;
           case "zuid":
-            currentLocation += 3;
+            if (Check(currentLocation))currentLocation += 3;
             break;
           case "oost":
-            currentLocation += 1;
+            if (Check(currentLocation))currentLocation += 1;
             break;
           case "west":
-            currentLocation -= 1;
+            if (Check(currentLocation))currentLocation -= 1;
             break;
         }
       } else {
-        feedback.innerHTML = "dat mag niet";
+        feedback.innerHTML = "ERROR ga...noord,west,zuid,oost";
         setTimeout(removeFeedback, 2000);
 
       }
@@ -84,9 +89,11 @@ function getInput(evt) {
     }
 
     if (inputArray[0] == "pak") {
-      console.log('ga wat pakken');
+      if(inputArray[1] == descriptions[currentLocation][1]) Inventory.push(inputArray[1]);
+      UpdateInventory();
       myInput.value = "";
     }
+
 
     if (inputArray[0] == "gebruik"){
       console.log('ga wat gebruiken');
@@ -103,19 +110,56 @@ function getInput(evt) {
 }
 
 function giveLocation() {
-  divLocation.innerHTML = locations[currentLocation];
-  myDescription.innerHTML = descriptions[currentLocation];
+  divLocation.innerHTML = locations[currentLocation] + " grid " + currentLocation;
+  myDescription.innerHTML = descriptions[currentLocation][0];
   imageLocation.src = "media/" + images[currentLocation];
   myDirections = "mogelijke richtingen zijn: ";
   for (let i = 0; i < directions[currentLocation].length; i++) {
     myDirections += "<li>" + directions[currentLocation][i] + "</li>";
   }
+  myDirections += showTreasure(currentLocation);
   myPossibilities.innerHTML = myDirections;
-  myInventory.innerHTML = "uw inventory is leeg";
+  UpdateInventory();
+}
+
+function UpdateInventory(){
+  if(Inventory.length > 0){
+    let items = "your inventory is:";
+    	for (let i = 0; i < Inventory.length; i++){
+        items += " " + Inventory[i];
+      } 
+      myInventory.innerHTML = items; 
+  }else 
+  {
+    myInventory.innerHTML = "uw inventory is leeg";
+  }
 }
 
 function removeFeedback() {
   feedback.innerHTML = "";
+}
+function showTreasure(currentLocation){
+  if(descriptions[currentLocation][1] != ""){
+  console.log(descriptions[currentLocation][1]);
+  treasure.src = "treasure/" + descriptions[currentLocation][2];
+  let treasureText = "Er zit een treasure in de kamer: " + descriptions[currentLocation][1];
+  return treasureText;
+  }
+  else{
+    treasure.src = "";
+    let treasureText = "";
+    return treasureText;
+  }
+}
+
+function Check(location){
+  if(descriptions[location][3] != ""){
+    for(let i = 0; i < Inventory.length; i++){
+      if(Inventory[i] == descriptions[location][3]){
+        return true;
+      }
+    } return false;
+  } return true;
 }
 
 giveLocation();
